05e4d082b0aed9ab7d00fefb31f91f9ca9c4a1a0
Saima Eva <saimayesmin99@gmail.com>
2023-08-05 00:42:00 +0600

    API Calling

--- js/fetch.js
+++ js/fetch.js
@@ -8,3 +8,94 @@
 // + fetch() returns a promise
 // - returned promise can only handle network error
 // - does not support all the older browser
+
+/* console.clear();
+fetch("https://jsonplaceholder.typicode.com/posts/1",{
+    method: "PUT",
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        id: 1,
+        title: 'fooAA',
+        body: 'barOO',
+        userId: 1,
+      }),
+
+})
+.then((res) => {
+    if (!res.ok){
+        const message = `Error: ${res.status}`;
+        throw new Error(message);
+        }
+        return res.json();
+})
+.then((res) => console.log(res))
+.catch((err) => console.log(err)); */
+
+//async await
+
+const makeRequest = async (url, config) => {
+  const res = await fetch(url, config);
+  if (!res.ok) {
+    const message = `Error : ${res.status}`;
+    throw new Error(message);
+  }
+  const data = await res.json();
+  return data;
+};
+
+const deleteData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+deleteData();
+
+/* const updateData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: "foomaraarrara",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+   updateData(); */
+
+/*    const sendData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+sendData();
+ */
+
+
+
+/* const getData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+getData(); */
