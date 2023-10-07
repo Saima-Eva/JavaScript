69d8c0bcfc6f0451ce853062f726384cab584884
Saima Eva <saimayesmin99@gmail.com>
2023-10-07 19:54:44 +0600

    json data

---
+++ JSON_TEST/index.js
@@ -0,0 +1,24 @@
+const data = require("./friends_data.json");
+//console.log(data);
+
+//console.log(data.friends[0]);
+
+
+/* data.friends[0].name = "ling shao";
+console.log(data.friends[0].name); */
+
+/* delete data.friends[0].name;
+console.log(data); */
+
+/* const data = require("./students_data.json");
+console.log(data.students[0].language[0]); */
+
+/* const data = require("./friends_data.json");
+
+for (x in data) {
+    console.log(x);
+    console.log(data[x]);
+} */
+
+
+//console.log(JSON.stringify(data2));
